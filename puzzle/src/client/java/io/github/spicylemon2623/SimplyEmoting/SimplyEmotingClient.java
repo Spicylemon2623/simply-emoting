23bbccadf8f3402eaa5ce05418992d71355d6e22
Spicylemon2623 <spicylemon2623@gmail.com>
2025-05-03 18:12:09 +0100

    please help

--- puzzle/src/client/java/io/github/spicylemon2623/SimplyEmoting/SimplyEmotingClient.java
+++ puzzle/src/client/java/io/github/spicylemon2623/SimplyEmoting/SimplyEmotingClient.java
@@ -1,18 +1,10 @@
 package io.github.spicylemon2623.SimplyEmoting;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import com.github.puzzle.core.loader.launch.provider.mod.entrypoint.impls.ClientModInitializer;
-import finalforeach.cosmicreach.CosmicReachFont;
-import finalforeach.cosmicreach.GameAssetLoader;
-
-import static io.github.spicylemon2623.SimplyEmoting.Constants.emojiSupport;
 
 public class SimplyEmotingClient implements ClientModInitializer {
     @Override
     public void onInit() {
         Constants.LOGGER.info("Simply Emoting INIT");
-        emojiSupport.Load(GameAssetLoader.loadAsset("simply_emoting:icons/font/emojis25.atlas")); //
-        emojiSupport.AddEmojisToFont(CosmicReachFont.getFont());
     }
 }
--- puzzle/src/main/java/io/github/spicylemon2623/SimplyEmoting/Constants.java
+++ puzzle/src/main/java/io/github/spicylemon2623/SimplyEmoting/Constants.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Constants {
-    public static final EmojiSupport emojiSupport = new EmojiSupport();
     public static final String MOD_ID = "simply-emoting";
     public static final Identifier MOD_NAME = Identifier.of(MOD_ID, "Simply Emoting");
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
